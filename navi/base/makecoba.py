aaa233fccc62075246c5edc38100ad50591eff15
Ivan Rocha <ivan.cr.neto@gmail.com>
2011-07-03 03:02:47 -0300

    changing the mainwindow class. Adjusting cover, abstract and structuring introduction of monograph

--- navi/base/makecoba.py
+++ navi/base/makecoba.py
@@ -1,15 +1,18 @@
+from xml.dom import minidom
 
 class Project:
     
-    def __init__(self, project_name, project_path):
-        self.name = project_name
+    def __init__(self, project_path):
         self.path = project_path
+        self.document = minidom.Document()
 
 class Makecoba:
 
     def __init__(self):
         pass
         
-    def new_project(self, string):
-        self.project = Project()
-        print 'Project ' +  string + ' Created!'
+    def new_project(self, project_path):
+        if project_path[:-4] != '.nav':
+            project_path += '.nav'
+        #self.project = Project(string)
+        #print 'Project ' +  string + ' Created!'
--- navi/gui/mainwindow.py
+++ navi/gui/mainwindow.py
@@ -19,21 +19,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.new_project)
         
     def new_project(self):
-        #self.file_dialog = QFileDialog(self)
-        #self.file_dialog.setAcceptMode(QFileDialog.AcceptSave)
-        #self.file_dialog.setFileMode(QFileDialog.AnyFile)
         import os
 
-        a = QFileDialog.getSaveFileNameAndFilter(self,
-            'Save Project File', os.path.expanduser('~'), 
-            'Navi Project file (*.nav)')
-        print a
-        #self.file_dialog = QFileDialog(self)
-        #self.file_dialog.setAcceptMode(QFileDialog.AcceptSave)
-        #self.file_dialog.setFileMode(QFileDialog.AnyFile)
-        #self.file_dialog.setDirectory(os.path.expanduser('~'))
-        #self.file_dialog.setNameFilter('Navi Project file (*.nav)')
-        #self.file_dialog.open()        
-        #a = self.file_dialog.selectedFiles()
-        #print a.last()
-        #file_name = getSaveFileNameAndFilter(None, 'Choose the file name', '/home', QString filter = QString(), QString initialFilter = QString(), Options options = 0)
+        file_name = QFileDialog.getSaveFileName(self, 'Save Project File',
+            os.path.expanduser('~'), 'Navi Project file (*.nav)')
+        makecoba = Makecoba()
+        makecoba.new_project(file_name)
