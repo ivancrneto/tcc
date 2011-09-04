1539880d3a9c3defb686ca8c5e0e2f1635b231ef
Ivan Rocha <ivan.cr.neto@gmail.com>
2011-09-04 15:59:46 -0300

    beginning filter_file in BioHandler class

--- navi/base/makecoba.py
+++ navi/base/makecoba.py
@@ -1,5 +1,9 @@
 from xml.dom import minidom
 from persistence import Persistence
+from Bio import SeqIO
+import os
+import glob
+
 
 class Project:
     '''
@@ -8,6 +12,10 @@ class Project:
     path
     database_path
     database_date
+    BioHandler 1
+    SimilarityMatrix 1
+    AdjacencyMatrix *
+    NeighbourhoodMatrix *
     '''
     
     def __init__(self, name, path, database_path):
@@ -31,6 +39,9 @@ class BioHandler:
     '''
     database_path
     database_date
+    Blast 1
+    Organism *
+    Matrix 1..*
     '''
 
     def __init__(self, database_path):
@@ -38,12 +49,71 @@ class BioHandler:
         self.filter_files()
         
     def filter_files(self):
+        #TODO: this should be done by persistence
+        for file_name in glob.glob(os.path.join('%s' % self.database_path, '*')):
+            for seq_record in SeqIO.parse(file_name, "genbank"):
+                print file_name
+                print seq_record.id #locus
+                print seq_record.description #definition
+                print seq_record.name
+                #print seq_record.locus
+                #print seq_record.gi
+                #print seq_record.keywords
+                print seq_record.seq.alphabet #alphabet
+                #print seq_record.organism
+                print dir(seq_record)
+                print seq_record.seq #sequence
+                print dir(seq_record.seq)
+                
+                #keywords
+                #gi
+                #organism
+                
+                #print repr(seq_record.seq)
+                #print len(seq_record)
+                #print '-------------------------------------------'
+                #print seq_record
+                #print '==========================================='
+                #$locus, $definition, $gi, $keywords, $organism, $sequence, $arquivo, $alphabet
+                #['__add__', '__class__', '__contains__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__getitem__', '__hash__', '__init__', '__iter__', '__len__', '__module__', '__new__', '__nonzero__', '__radd__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_per_letter_annotations', '_seq', '_set_per_letter_annotations', '_set_seq', 'annotations', 'dbxrefs', 'description', 'features', 'format', 'id', 'letter_annotations', 'lower', 'name', 'seq', 'upper']
+                #my $arquivo = $fileName;
+				
+				#my $locus = $seq->display_id;
+				#my $definition = $seq->desc;
+				#my $gi = $seq->primary_id;
+				#my $keywords = $seq->keywords;
+				#my $alphabet = $seq->alphabet;
+				#my $sequence = $seq->seq;
+
+        
+class Organism:
+    '''
+    description
+    name
+    Sequence 1..*
+    '''
+    
+    def __init__(self):
         pass
     
     
+class Sequence:
+    '''
+    ec
+    gi
+    complement
+    community
+    name
+    description
+    locus
+    file
+    alphabet
+    code
+    Organism 1
+    '''
     
-    
-    
+    def __init__(self):
+        pass
     
     
     
